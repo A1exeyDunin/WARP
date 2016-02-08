e9fdb7fefde1692b96d07de2fe710b45aa09fba0
BblcpuCaHKu <nagibator631@mail.ru>
2016-02-08 16:14:18 +0200

     

--- connect_bd.php
+++ connect_bd.php
@@ -1,22 +1,14 @@
-       
+<?php
 
- <?php
-        
-function connect()
-{
-$mysqli = mysqli_connect('localhost', 'Admin', '', 'bd1');
-{
-    if (!$mysqli)  { echo "error in connet <br/>" . mysqli_connect_error(); }
-        else     { echo "connect success <br/>"; }
+function connect() {
+    $mysqli = mysqli_connect('localhost', 'Admin', '', 'bd1');
+    {
+        if (!$mysqli) {
+            echo "error in connet <br/>" . mysqli_connect_error();
+        } else {
+            echo "connect success <br/>";
+        }
         return $mysqli;
+    }
 }
-}
-
-    
-            
-            
-      
-            
-            
-            
 ?>
