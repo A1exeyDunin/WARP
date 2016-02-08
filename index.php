f467b42f43eb16afea6d34cee44fc1197da06bc1
BblcpuCaHKu <nagibator631@mail.ru>
2016-02-08 16:13:45 +0200

    а тут теперь похер на эхо

--- index.php
+++ index.php
@@ -1,25 +1,19 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <?php
+
 include 'connect_bd.php';
 include 'functions.php';
 include 'articles.php';
+
+
+
+
+$mysqli = connect();
+
+$mydata = take($mysqli);
+
+echo $mydata['title'] . "<br/>" . $mydata['text'];
+
+
+
 ?>
 
-        <?php
-        
-        $mysqli = connect();
-        
-        $mydata = take($mysqli);
-        
-        echo $mydata['title'];
-        echo "<br/>";
-        echo $mydata['text'];
-        
-        ?>
-
-    
