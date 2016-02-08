d8e7742876c4474e90d48f40ccfd00f6dd7132a7
BblcpuCaHKu <nagibator631@mail.ru>
2016-02-08 15:01:51 +0200

    упаковал в фунцию

--- connect_bd.php
+++ connect_bd.php
@@ -1,16 +1,21 @@
        
 
  <?php
-        $mysqli = mysqli_connect('localhost', 'Admin', '', 'bd1');
-        if (!$mysqli) { echo "error" . mysqli_connect_error(); }
-        else { return $mysqli; } 
         
-            
+function connect()
+{
+$mysqli = mysqli_connect('localhost', 'Admin', '', 'bd1');
+{
+    if (!$mysqli)  { echo "error in connet <br/>" . mysqli_connect_error(); }
+        else     { echo "connect success <br/>"; }
+        return $mysqli;
+}
+}
 
-            mysqli_close($mysqli);
+    
             
             
-            
+      
             
             
             
