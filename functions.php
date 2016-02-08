f385bd8f99044cfee7ee966c036bc40e3c0e1540
BblcpuCaHKu <nagibator631@mail.ru>
2016-02-08 17:16:41 +0200

    забор с бд

--- functions.php
+++ functions.php
@@ -3,10 +3,10 @@
 function take($mysqli) {
     $result = mysqli_query($mysqli, 'SELECT id, title, text FROM articles') or die(mysqli_error());
 
-    while ($myrow = mysqli_fetch_assoc($result)) {
-        echo $myrow['title'] . "<br/>" . $myrow['text'] . "<br/>";
-    }
+    $myrow = mysqli_fetch_assoc($result);
+    
+    return $myrow;
 }
 
-return $myrow;
+
 ?>
