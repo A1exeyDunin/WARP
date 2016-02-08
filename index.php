7d4b8b09702fa1bbb157f24fb3211439310c1049
BblcpuCaHKu <nagibator631@mail.ru>
2016-02-08 17:17:07 +0200

    тут инклуд

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include 'connect_bd.php';
 include 'functions.php';
-include 'articles.php';
+
 
 
 
@@ -11,7 +11,10 @@ $mysqli = connect();
 
 $mydata = take($mysqli);
 
-echo $mydata['title'] . "<br/>" . $mydata['text'];
+include 'view.php';
+
+
+
 
 
 
