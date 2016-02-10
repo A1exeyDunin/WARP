12d9aee3a845a0c493b3b6ac7ca9515bafb2c1c7
BblcpuCaHKu <nagibator631@mail.ru>
2016-02-10 15:59:27 +0200

    1

---
+++ single_view.php
@@ -0,0 +1,25 @@
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        
+        <title></title>
+        <link href="style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        
+        <div class="content">
+        <?php
+        
+       // echo $mydata['title'] . "<br/>" . $mydata['text'] . "<br/>";
+        
+        ?>
+        
+        
+            <h3><?=$mydata['title']?></h3>
+            <p><?=$mydata['text']?></p>
+            
+            
+        </div>
+    </body>
+</html>
