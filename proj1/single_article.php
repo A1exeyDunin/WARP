c077f9ecc7c9d560b5d591d7fef7226719e04672
BblcpuCaHKu <nagibator631@mail.ru>
2016-02-11 17:30:31 +0200

    1

--- proj1/single_article.php
+++ proj1/single_article.php
@@ -3,31 +3,21 @@
 include 'functions/connect_bd.php';
 include 'functions/functions.php';
 
-
-
-
-
 $mysqli = connect();
+
 
 $id = $_GET["id"];
 
-
 $mydata = take1($mysqli, $id);
 
+$mydata1 = comment($mysqli, $id);
 
 include 'view/single_view.php';
-
-
-
 
 $author = $_POST["author"];
 $page_id = $_POST["page_id"];
 $comment = $_POST["comment"];
 
-
 $insert = insert($mysqli, $author, $page_id, $comment);
-
-$mydata1 = comment($mysqli, $page_id);
-
 
 ?>
